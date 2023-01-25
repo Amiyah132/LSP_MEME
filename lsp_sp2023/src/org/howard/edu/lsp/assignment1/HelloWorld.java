f1c618f05b216665e5398391c8fdd90163f7357c
Amiyah132 <Amiyah.brown@bison.howard.edu>
2023-01-25 13:30:58 -0500

    My first commit to GitHub.

---
+++ lsp_sp2023/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+*  Name:Amiyah Brown
+*/
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
